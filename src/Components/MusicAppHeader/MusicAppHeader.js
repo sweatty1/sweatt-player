9c76581f1e078c23a8fe7bf90c4dae8675647497
sweatty1 <8526024+sweatty1@users.noreply.github.com>
2020-09-15 22:33:19 -0500

    Much cleaner way for handling context in the header
    Using useContext in the app header instead of relying on passing
    function into functions using html consumers much much cleaner

--- src/Components/MusicAppHeader/MusicAppHeader.js
+++ src/Components/MusicAppHeader/MusicAppHeader.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ListItem, List, ListItemText, Menu, MenuItem } from '@material-ui/core';
-import { MusicInfoContext} from '../../Contexts/MusicInfoContext';
+import { MusicInfoContext } from '../../Contexts/MusicInfoContext';
 import { CurrentlyPlayingContext } from '../../Contexts/CurrentlyPlayingContext';
 var { dialog } = window.require('electron').remote;
 
-export default function MusicAppHeader(props) {
+export default function MusicAppHeader() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const currentlyPlayingContext = useContext(CurrentlyPlayingContext);
+  const musicInfoContext = useContext(MusicInfoContext);
 
   const handleOpenMenu = (event) => {
     setAnchorEl(event.currentTarget)
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
 
-  const handleClearSelectedMusic = (clearSelected) => {
-    clearSelected();
+  const handleClearSelectedMusic = () => {
+    currentlyPlayingContext.clearSelectedMusic();
     setAnchorEl(null);
   }
 
-  const handleClearAllMusic = (clearSelected, clearLoaded) => {
-    clearSelected();
-    clearLoaded();
+  const handleClearAllMusic = () => {
+    currentlyPlayingContext.clearSelectedMusic();
+    musicInfoContext.clearLoadedMusic();
     setAnchorEl(null);
   }
 
-  const handleSetBaseFolderForMusic = (readMusicFolder, clearSelected) => {
+  const handleSetBaseFolderForMusic = () => {
     dialog.showOpenDialog({ title: "Select Music Folder", properties: ['openDirectory']}).then((data) => {
       if (data.filePaths && data.filePaths[0] !== undefined) {
         const folder = data.filePaths[0];
-        readMusicFolder(folder);
-        clearSelected();
+        musicInfoContext.readMusicFolder(folder);
+        currentlyPlayingContext.clearSelectedMusic();
       } else {
         console.log("No folder selected");
       }
@@ -39,47 +40,34 @@ export default function MusicAppHeader(props) {
     });
   }
 
-    return (
-        <header className="App-header">
-          <MusicInfoContext.Consumer>
-              {({musicFolder}) => (
-                <List component="nav">
-                  <ListItem button
-                    aria-haspopup="true"
-                    aria-controls="option-menu"
-                    onClick={handleOpenMenu}
-                  >
-                    <ListItemText primary="Data Controls" secondary={musicFolder} />
-                  </ListItem>
-                </List>
-              )}
-            </MusicInfoContext.Consumer>
-            
-            <MusicInfoContext.Consumer>
-              {({clearLoadedMusic, readMusicFolder}) => (
-                <CurrentlyPlayingContext.Consumer>
-                  {({clearSelectedMusic}) => (
-                    <Menu 
-                    id="option-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                  >
-                    <MenuItem onClick={() => handleSetBaseFolderForMusic(readMusicFolder, clearSelectedMusic)}>
-                    Set New Root Folder
-                    </MenuItem>
-                    <MenuItem onClick={() => handleClearSelectedMusic(clearSelectedMusic)}>
-                    Deselect CurrentSong
-                    </MenuItem>
-                    <MenuItem onClick={() => handleClearAllMusic(clearSelectedMusic, clearLoadedMusic)}>
-                    Clear Entire State
-                    </MenuItem>
-                  </Menu>
-                  )}
-                </CurrentlyPlayingContext.Consumer>
-              )}
-             </MusicInfoContext.Consumer>
-        </header>
-    )
+  return (
+    <header className="App-header">
+      <List component="nav">
+        <ListItem button
+          aria-haspopup="true"
+          aria-controls="option-menu"
+          onClick={handleOpenMenu}
+        >
+          <ListItemText primary="Data Controls" secondary={musicInfoContext.musicFolder} />
+        </ListItem>
+      </List>
+      <Menu 
+        id="option-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => handleSetBaseFolderForMusic()}>
+        Set New Root Folder
+        </MenuItem>
+        <MenuItem onClick={() => handleClearSelectedMusic()}>
+        Deselect CurrentSong
+        </MenuItem>
+        <MenuItem onClick={() => handleClearAllMusic()}>
+        Clear Entire State
+        </MenuItem>
+      </Menu>
+    </header>
+  )
 }
