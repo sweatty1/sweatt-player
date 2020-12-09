ff9efb01e589371b7cbdf24b3db49aaab13981ca
sweatty1 <8526024+sweatty1@users.noreply.github.com>
2020-12-09 12:04:26 -0600

    Changes to the css to limit content to a single page
    Removed a container which was conflicting with custom css

--- src/Components/MusicInfoDisplay/MusicInfoDisplay.js
+++ src/Components/MusicInfoDisplay/MusicInfoDisplay.js
@@ -14,28 +14,28 @@ const styles = makeStyles(theme => ({
 
   // A note this extra container is needed as per https://github.com/mui-org/material-ui/issues/7466
   // where in material UI the grids seem to extend a little outwards beyond the screen causes a scrollbar to appear that moves slightly
+  // no longer needed but good to know about as container messed up keeping things to one page and getting the songlist to scoll
+  // although having container did added 12px to each item which is now done manually
 const MusicInfoDisplay = (props) => {
     const classes = styles;
     return (
-        <Box padding={2}>
-            <Grid container spacing={3}>
-                <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <ArtistList/>
-                </Paper>
-                </Grid>
-                <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <AlbumList/>
-                </Paper>
-                </Grid>
-                <Grid item xs={4}>
-                <Paper className={classes.paper}>
-                    <SongList/>
-                </Paper>
-                </Grid>
+        <Grid className="flex-control-row" spacing={3}>
+            <Grid className="flex-control-column" item style={{padding: '12px'}} xs={4}>
+            <Paper classes={{root: "flex-control-column", paper: classes.paper}}>
+                <ArtistList/>
+            </Paper>
             </Grid>
-        </Box>
+            <Grid className="flex-control-column" item style={{padding: '12px'}} xs={4}>
+            <Paper classes={{root: "flex-control-column", paper: classes.paper}}>
+                <AlbumList/>
+            </Paper>
+            </Grid>
+            <Grid className="flex-control-column" item style={{padding: '12px'}} xs={4}>
+            <Paper classes={{root: "flex-control-column", paper: classes.paper}}>
+                <SongList/>
+            </Paper>
+            </Grid>
+        </Grid>
     )
 }
 export default MusicInfoDisplay;
